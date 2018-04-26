0880c127fe7903ba98f00e3edb5499a600bc8ca9
EDMAR PEREIRA <pedmar@br.ibm.com>
2018-04-26 10:33:09 -0300

    Added first file

---
+++ app.js
@@ -0,0 +1,22 @@
+var time = 0;
+
+console.log(__dirname);
+console.log(__filename);
+
+console.log('Timer start');
+
+setTimeout(function(){
+    console.log('3 seconds have passed');
+}, 1000);
+
+console.log('Start a new timer start');
+
+
+var timer = setInterval(function(){
+    time += 1;
+    console.log(time +' seconds have passed');
+    if (time > 5){
+        clearInterval(timer);
+    }
+},2000);
+
