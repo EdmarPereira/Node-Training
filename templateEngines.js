bf6518752d1ada508cd182496ce289b6d5badc7e
EDMAR PEREIRA <pedmar@br.ibm.com>
2018-04-26 10:56:07 -0300

    Template engies

--- templateEngines.js
+++ templateEngines.js
@@ -12,7 +12,7 @@ app.get('/contact', function(req, res){
 });
 
 app.get('/profile/:name', function(req, res){
-    var data = {age: 29, job: 'developer'};
+    var data = {age: 29, job: 'developer', hobbies: ['eating', 'ride', 'fishing']};
     res.render('profile', {person: req.params.name, data: data});
 });
 app.listen(3000);
