ce9829ed78b151c299ed3244c16829012c33903a
Edmar <edmar.pereira80@gmail.com>
2018-05-30 09:59:06 -0300

    balta Mongoose fase

--- Balta.Io Training/node-str/src/app.js
+++ Balta.Io Training/node-str/src/app.js
@@ -2,21 +2,22 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+//conecção com db
+mongoose.connect('mongodb://nodejs_balta1:nodejs_balta1@ds050559.mlab.com:50559/nodejs_balta')
 
 // carregas as rotas do projeto
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route')
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ 
+app.use(bodyParser.urlencoded({
     extended: false
 }));
-
-
-
 
 
 app.use('/', indexRoute);
